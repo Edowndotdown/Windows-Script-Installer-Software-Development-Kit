178b87e75a8cac980ea9890fd83d2588cde2da5c
Edowndotdown <115327261+Edowndotdown@users.noreply.github.com>
2024-08-08 16:25:30 -0400

    1

---
+++ ExampleSDK/setup.bat
@@ -0,0 +1,7 @@
+@echo off
+title setup
+echo Windows Script Installer Has started this Setup script.
+echo WSI also allows Setup as a .exe file.
+echo Remember to open URL folder and make a file containing a download URL.
+pause
+exit
